eb27316c89ce5dcea7ecef8f3eeab996c539f7a0
colemanja91 <colemanja91@gmail.com>
2023-09-30 08:07:58 -0400

    use public for mp3 files

--- src/components/PlayTtsButton.js
+++ src/components/PlayTtsButton.js
@@ -1,7 +1,7 @@
-import {React} from 'react';
+import React from "react";
 
 export default function PlayTtsButton({ ttsFilePath }) {
   return (
-    <audio src={`file://${ttsFilePath}`} type="audio/mpeg" controls autoplay></audio>
+    <audio src={`shared_data/${ttsFilePath}`} type="audio/mp3" controls autoplay></audio>
   )
 }
